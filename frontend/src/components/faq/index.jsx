c9af4ec5431137969b3cf55e1d40359b266cff73
Akuya Ekorot <akuyaekorot@gmail.com>
2022-11-19 21:36:58 +0300

    refactor: employ new project structure

---
+++ frontend/src/components/faq/index.jsx
@@ -0,0 +1,16 @@
+import FaqHero from "./FaqHero";
+import FaqGetInTouch from "./FaqGetInTouch";
+import FaqNewsletter from "./FaqNewsletter";
+import QuestionSection from "./QuestionSection";
+
+const Faq = () => {
+  return (
+    <>
+      <FaqHero />
+      <QuestionSection />
+      <FaqNewsletter />
+    </>
+  );
+};
+
+export default Faq;
