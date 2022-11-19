3b5fa1cca886d1128d353354e22dcbb852ee0dde
Benedicta <benedictauche.onyebuchi@gmail.com>
2022-11-19 14:49:51 +0100

    [ADD]: Added new features

---
+++ Frontend/noxus/src/components/careers-page/Main.js
@@ -0,0 +1,66 @@
+import Intro from './Main';
+
+const Main = () => {
+    return (
+        <div>
+            <Intro />
+            <p>Careers at Nexus</p>
+            <div className="flex">
+                <div class="first-div">
+                    <div>
+                        <p>TECH DEPARTMENT</p>
+                        <h2>Senior software developer</h2>
+                        <p>NGN 600,000 - NGN 800,000</p>
+                        <span>Apply Now</span>
+                    </div>
+                    <div>
+                        <p>HR DEPARTMENT</p>
+                        <h2>Talent manager</h2>
+                        <p>NGN 600,000 - NGN 800,000</p>
+                        <span>Apply Now</span>
+                    </div>
+                    <div>
+                        <p>DESIGN</p>
+                        <h2>Senior product designer</h2>
+                        <p>NGN 600,000 - NGN 800,000</p>
+                        <span>Apply Now</span>
+                    </div>
+                    <div>
+                        <p>DESIGN</p>
+                        <h2>Brand identit designer</h2>
+                        <p>NGN 600,000 - NGN 800,000</p>
+                        <span>Apply Now</span>
+                    </div>
+                    <div>
+                        <p>TECH DEPARTMENT</p>
+                        <h2>Senior software developer</h2>
+                        <p>NGN 600,000 - NGN 800,000</p>
+                        <span>Apply Now</span>
+                    </div>
+                </div>
+                <div class="second-div">
+                    <div>
+                        <h2>Pick a plan and create your AI generated Avatars</h2>
+                    </div>
+                    <div>
+                        <p>Our vision to make it easier for people to be able to envision and see 
+                            themselves in the different ways they can imagine one Avatar at a 
+                            time. We build our customers a world-class avatar systems their users
+                            deserve.
+                        </p>
+                        <p>
+                            Noxus help users create an avatar they can use across many virtual
+                            experiences and give developers an easy-to-use tool for building
+                            avatar-based experiences.
+                        </p>
+                    </div>
+                    <div>
+                        <img src="..images/tapart2.png" alt="background logo" />
+                    </div>
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default Main;
