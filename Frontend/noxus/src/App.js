c3eb768f2b2e8ac10acce2812116df4bb0cccadd
Headmaster <aliubashirtaiye2002@gmail.com>
2022-11-19 14:53:51 +0100

    Fixed Terms Css

--- Frontend/noxus/src/App.js
+++ Frontend/noxus/src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/landingPage/Navbar/Navbar'
 import './App.css'
 import Ttilp from './components/TTILP/Ttilp'
 import Term from './components/TermsPage/Terms.jsx'
+import Error from './components/404page/404.jsx'
 import './App.css'
 
 function App() {
