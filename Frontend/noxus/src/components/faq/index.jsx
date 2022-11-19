4922a7cdf22834dc8aa1eb6438d178893bc74bd1
Akuya Ekorot <akuyaekorot@gmail.com>
2022-11-19 14:53:38 +0300

    feat: add newsletter subscription section

--- Frontend/noxus/src/components/faq/index.jsx
+++ Frontend/noxus/src/components/faq/index.jsx
@@ -50,6 +50,26 @@ const Faq = () => {
           </a>
         </div>
       </section>
+      <section className="flex flex-col gap-[24px] bg-[#F9F9FB] py-[40px] px-[24px]">
+        <div className="flex flex-col gap-[24px]">
+          <img src="/tapart.svg" height={`40px`} width={`40px`} />
+          <div className="flex flex-col gap-[8px]">
+            <p className="text-[#201F23] font-jakarta font-bold">
+              Subscribe to our newsletter for info for new avatar scenes, blogs
+              and updates
+            </p>
+            <p className="text-[#605E65] font-nunito">
+              We'd send a confirmation email to you
+            </p>
+          </div>
+        </div>
+        <div className="flex flex-col gap-[16px]">
+          <div className="h-[52px] w-full bg-[#8B70E9] rounded"></div>
+          <p className="text-[#AFB6B6] text-sm">
+            By subscribing you agree to accept newsletter and Emails from us.
+          </p>
+        </div>
+      </section>
     </>
   );
 };
