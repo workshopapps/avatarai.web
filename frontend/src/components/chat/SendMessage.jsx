534c965b4b4cf962d11512c4f03b54779078ac0f
Akuya Ekorot <akuyaekorot@gmail.com>
2022-11-20 02:57:09 +0300

    design: send message component (not done)

---
+++ frontend/src/components/chat/SendMessage.jsx
@@ -0,0 +1,13 @@
+const SendMessage = () => {
+  return (
+    <div className="flex flex-col gap-20">
+      <div>
+        <div></div>
+        <div></div>
+      </div>
+      <div>
+        <p>Send Message</p>
+      </div>
+    </div>
+  );
+};
