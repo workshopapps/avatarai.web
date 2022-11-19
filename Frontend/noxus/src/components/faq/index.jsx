7dba4691af28d8f9289f6b8f6f7c2e80ff023fae
Akuya Ekorot <akuyaekorot@gmail.com>
2022-11-19 15:07:43 +0300

    docs: add comments to sections of the page

--- Frontend/noxus/src/components/faq/index.jsx
+++ Frontend/noxus/src/components/faq/index.jsx
@@ -11,6 +11,7 @@ const Faq = () => {
 
   return (
     <>
+      {/* Hero Section */}
       <section className="flex flex-col gap-[20px] items-center p-[24px] bg-cover bg-center bg-[url('https://s3-alpha-sig.figma.com/img/67ae/db4d/827320a8065271b507f5f40f95906d6f?Expires=1669593600&Signature=X4aMQMssyYHb-msSCgO1FT9ym4FF2B4sfej9jUoMVFYCZpXlL2ddPbMc5Ux1o0-NSqyzonzG72MT8RUVhZYVClWFKNXCcGeB8wB-p5UcuIEYdoJIYsNP1NJMZDD7u5oQDvFZs-ouRQELoH7f4Hsr2Ll~I2Adt6fUb~XgMq7V4Uwbgd4rgs8uscstYij7P938JgYmsK3nIGTgfVY9Um38aRUTQTkzigdfInmFRsi0oQwNKhvNnSloyNy-1xLP3EDl6G~~a3DkcAw2eaL4vEEgu3kdwVKCB6mKIM1XJKcYYppVlCwuGGseQvDCHoIq~m16qrguQKKUR~E2t-ONhxsQug__&Key-Pair-Id=APKAINTVSUGEWH5XD5UA')]">
         <header className="flex flex-col gap-[8px] text-[20px] items-center font-nunito text-white">
           <h1 className="font-bold ">Frequently Asked Questions</h1>
@@ -18,20 +19,26 @@ const Faq = () => {
         </header>
         <div className="h-[52px] w-full bg-white rounded"></div>
       </section>
+
+      {/* Details Section */}
       <section className="font-nunito flex flex-col items-center px-[24px] py-[80px] gap-[94px]">
+        {/* FAQs and Contact Us Toggle */}
         <div className="h-[52px] w-full bg-[#8B70E9] rounded"></div>
+        {/* Expandable cards Section */}
         <div className="w-full">
           {data.map((_, i) => (
             <div
               key={i}
               className=" flex flex-col gap-[8px] py-[24px] border-b"
             >
+              {/* Card Heading and Symbol */}
               <div onClick={handleClick} className="flex justify-between ">
                 <h2 className="w-[260px] text-[20px] font-medium">
                   What are digital avatars used for?
                 </h2>
                 <p className="text-[32px] font-bold">{hidden ? "+" : "-"}</p>
               </div>
+              {/* Card Description */}
               <p className={`${hidden ? "hidden" : "block"} w-[260px]`}>
                 Digital avatars can serve a number of purposes, including
                 representing a person, an online friend, a brand, or an
@@ -40,6 +47,7 @@ const Faq = () => {
             </div>
           ))}
         </div>
+        {/* Get in Touch Section */}
         <div className="flex flex-col gap-[42px]">
           <div className="flex flex-col items-center">
             <h3 className="font-medium text-[20px]">Still have a question?</h3>
@@ -50,6 +58,8 @@ const Faq = () => {
           </a>
         </div>
       </section>
+
+      {/* Newsletter Subscription Section */}
       <section className="flex flex-col gap-[24px] bg-[#F9F9FB] py-[40px] px-[24px]">
         <div className="flex flex-col gap-[24px]">
           <img src="/tapart.svg" height={`40px`} width={`40px`} />
