0cd49d5583fc82b8e23801587ba5add4a6974a6f
Headmaster <aliubashirtaiye2002@gmail.com>
2022-11-19 13:51:09 +0100

    ft:update terms page

--- Frontend/noxus/src/App.js
+++ Frontend/noxus/src/App.js
@@ -2,10 +2,11 @@ import React from 'react'
 import Navbar from './components/landingPage/Navbar/Navbar'
 import './App.css'
 import Ttilp from './components/TTILP/Ttilp'
+import Term from './components/TermsPage/Terms.jsx'
 import './App.css'
 
 function App() {
-  return <Navbar />
+  return <Term />
 }
 
 export default App
