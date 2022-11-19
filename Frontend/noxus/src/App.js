a5f308244fd8bd69f59232755b3a24781dbf6644
Oke Timilehin <oketimilehin32@gmail.com>
2022-11-19 06:51:29 +0100

    course detail

--- Frontend/noxus/src/App.js
+++ Frontend/noxus/src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import CoursePage from "./components/courses/CoursePage";
+import Navbar from "./components/courses/Navbar";
+import CourseDetail from "./components/courses/CourseDetail";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<CoursePage /> } />
+        <Route path="details/:detailId" element={<CourseDetail />} />
+      </Routes>
     </div>
   );
 }
