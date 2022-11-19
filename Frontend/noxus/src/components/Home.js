0490c8834d67b69578a66ac1033c520f1b85298c
VicNW <nwosuvictor54@gmail.com>
2022-11-19 11:09:30 +0400

    frist commit, designed the body of the text to image landing page

---
+++ Frontend/noxus/src/components/Home.js
@@ -0,0 +1,11 @@
+import './Hero.css';
+
+function Home() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Home
---
+++ Frontend/noxus/src/components/TTILP/Button.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './TTILP.css';
+function Button() {
+  return (
+    <a href='https://google.com'
+    >
+      <button className='vic_button'>
+        
+        Generate image
+         
+         </button>
+         </a>
+  )
+}
+
+export default Button
