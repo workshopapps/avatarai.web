8b01f83630e99d5bce3b6e867d3922e3b7b7d43e
VeedJohnson <48daveed@gmail.com>
2022-11-19 09:22:15 +0100

    feat: added team data and images

---
+++ Frontend/noxus/src/components/aboutus/data.js
@@ -0,0 +1,22 @@
+const TEAM = [
+    {
+        name: "Name Surname",
+        role: "Product designer",
+        img: "./img/member1.png"
+    },
+    {
+        name: "Name Surname",
+        role: "Product designer",
+        img: "./img/member2.png"
+    },
+    {
+        name: "Name Surname",
+        role: "Product designer",
+        img: "./img/member3.png"
+    },
+    {
+        name: "Name Surname",
+        role: "Product designer",
+        img: "./img/member4.png"
+    },
+]
