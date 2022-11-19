37059211e9d2da0984fc73cf875e38d48a8d2fd7
Akuya Ekorot <akuyaekorot@gmail.com>
2022-11-20 00:46:42 +0300

    design: make logo component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import Logo from "./components/chat/Logo";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <>
+      {/*<App />*/}
+      <Logo />
+    </>
   </React.StrictMode>
-)
+);
