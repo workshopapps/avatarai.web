f7d34483a28d10219e3ec02d7c35f071a1879df3
Akuya Ekorot <akuyaekorot@gmail.com>
2022-11-20 01:01:54 +0300

    design: make the navigation link

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import Logo from "./components/chat/Logo";
+import NavigationLink from "./components/chat/NavigationLink";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
@@ -9,6 +10,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
     <>
       {/*<App />*/}
       <Logo />
+      <NavigationLink text={`Create avatar`} image={`/tapart.svg`} />
     </>
   </React.StrictMode>
 );
--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,15 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-  "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
         nunito: ["Nunito"],
+        "nunito-sans": ["Nunito Sans"],
         jakarta: ["'Plus Jakarta Sans'"],
       },
     },
   },
   plugins: [],
-}
+};
