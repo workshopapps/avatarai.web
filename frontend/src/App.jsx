60f83d3e3430fc361f13eea8e39f1e04540e76e8
VicNW <nwosuvictor54@gmail.com>
2022-11-20 22:23:33 +0400

    .

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -32,7 +32,7 @@ import CheckEmail from './components/LoginPage/CheckEmail';
 import ResetPassword from './components/LoginPage/ResetPassword';
 import Chat from './components/chat/ChatPage';
 import LandingPage from './components/landing-page/LandingPageC';
-import CareerPage from './components/careers-page/careerPage'
+import CareerPage from './components/careers-page/careerPage';
 
 export default function App() {
   return (
