f32b2d4efbff016b14ff51ce88dfc211b37c6c08
Akuya Ekorot <akuyaekorot@gmail.com>
2022-11-20 14:09:51 +0300

    feat: add modal to chat page

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import App from "./App";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <>
-      <App />
-    </>
+    <App />
   </React.StrictMode>
 );
