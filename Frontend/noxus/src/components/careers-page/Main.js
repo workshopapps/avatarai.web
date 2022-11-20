0bdd3e261a835404fbc17791f6c1677ed1c470e6
Benedicta <benedictauche.onyebuchi@gmail.com>
2022-11-20 14:05:08 +0100

    [ADD]: Added styling

--- Frontend/noxus/src/components/careers-page/Main.js
+++ Frontend/noxus/src/components/careers-page/Main.js
@@ -6,58 +6,79 @@
 const Main = () => {
     return (
         <div>
-            <p className="text-center text-3xl font-semibold mt-3">Careers at Nexus</p>
-            <div className="flex justify-center flex-row">
-                <div id="first-div" className="">
-                    <div>
-                        <p>TECH DEPARTMENT</p>
-                        <h2>Senior software developer</h2>
-                        <p>NGN 600,000 - NGN 800,000</p>
-                        <span>Apply Now</span>
+            <p className="text-center text-3xl font-semibold mt-3 mb-8">Careers at Nexus</p>
+            <div className="flex flex-row justify-evenly">
+                <div id="first-div">
+                    <div className="flex flex-row space-x-10 mb-6 items-center justify-between">
+                        <div className="text-left">
+                            <p className="text-xs text-[#808080] mb-3">TECH DEPARTMENT</p>
+                            <h2 className="font-semibold mb-3">Senior software developer</h2>
+                            <p className="text-emerald-500 text-xs font-semibold mb-3">NGN 600,000 - NGN 800,000</p>
+                        </div>
+                        <div className="items-end">
+                            <span className="text-purple-500 text-sm font-semibold">Apply Now</span>
+                        </div>
                     </div>
-                    <div>
-                        <p>HR DEPARTMENT</p>
-                        <h2>Talent manager</h2>
-                        <p>NGN 600,000 - NGN 800,000</p>
-                        <span>Apply Now</span>
+                    <div className="flex flex-row space-x-10 mb-6 items-center justify-between">
+                        <div className="text-left">
+                            <p className="text-xs text-[#808080] mb-3">HR DEPARTMENT</p>
+                            <h2 className="font-semibold mb-3">Talent manager</h2>
+                            <p className="text-emerald-500 text-xs font-semibold mb-3">NGN 600,000 - NGN 800,000</p>
+                        </div>
+                        <div className="items-end">
+                            <span className="text-purple-500 text-sm font-semibold">Apply Now</span>
+                        </div>
                     </div>
-                    <div>
-                        <p>DESIGN</p>
-                        <h2>Senior product designer</h2>
-                        <p>NGN 600,000 - NGN 800,000</p>
-                        <span>Apply Now</span>
+                    <div className="flex flex-row space-x-10 mb-6 items-center justify-between">
+                        <div className="text-left">
+                            <p className="text-xs text-[#808080] mb-3">DESIGN</p>
+                            <h2 className="font-semibold mb-3">Senior product designer</h2>
+                            <p className="text-emerald-500 text-xs font-semibold mb-3">NGN 600,000 - NGN 800,000</p>
+                        </div>
+                        <div className="items-end">
+                            <span className="text-purple-500 text-sm font-semibold">Apply Now</span>
+                        </div>
                     </div>
-                    <div>
-                        <p>DESIGN</p>
-                        <h2>Brand identity designer</h2>
-                        <p>NGN 600,000 - NGN 800,000</p>
-                        <span>Apply Now</span>
+                    <div className="flex flex-row space-x-10 mb-6 items-center justify-between">
+                        <div className="text-left">
+                            <p className="text-xs text-[#808080] mb-3">DESIGN</p>
+                            <h2 className="font-semibold mb-3">Brand identity designer</h2>
+                            <p className="text-emerald-500 text-xs font-semibold mb-3">NGN 600,000 - NGN 800,000</p>
+                        </div>
+                        <div className="items-end">
+                            <span className="text-purple-500 text-sm font-semibold">Apply Now</span>
+                        </div>
                     </div>
-                    <div>
-                        <p>TECH DEPARTMENT</p>
-                        <h2>Senior software developer</h2>
-                        <p>NGN 600,000 - NGN 800,000</p>
-                        <span>Apply Now</span>
+                    <div className="flex flex-row space-x-10 mb-6 items-center justify-between">
+                        <div className="text-left">
+                            <p className="text-xs text-[#808080] mb-3">TECH DEPARTMENT</p>
+                            <h2 className="font-semibold mb-3">Senior software developer</h2>
+                            <p className="text-emerald-500 text-xs font-semibold mb-3">NGN 600,000 - NGN 800,000</p>
+                        </div>
+                        <div className="items-end">
+                            <span className="text-purple-500 text-sm font-semibold">Apply Now</span>
+                        </div>
                     </div>
                 </div>
-                <div class="second-div">
+                <div id="second-div" className="text-right">
+                    <p>We Are Hiring</p>
                     <div>
                         <h2>Pick a plan and create your AI generated Avatars</h2>
                     </div>
                     <div>
-                        <p>Our vision to make it easier for people to be able to envision and see 
-                            themselves in the different ways they can imagine one Avatar at a 
-                            time. We build our customers a world-class avatar systems their users
+                        <p>Our vision to make it easier for people to be able to envision and see<br/>
+                            themselves in the different ways they can imagine one Avatar at a<br/>
+                            time. We build our customers a world-class avatar systems their users<br/>
                             deserve.
                         </p>
                         <p>
-                            Noxus help users create an avatar they can use across many virtual
-                            experiences and give developers an easy-to-use tool for building
-                            avatar-based experiences.
+                            Noxus help users create an avatar they can use across many virtual<br/>
+                            experiences and give developers an easy-to-use tool for building<br/>
+                            avatar-based experiences.<br/>
                         </p>
                     </div>
                     <div>
-                        <img src="..images/tapart2.png" alt="background logo" />
+                        <img src="../images/tapart2.png" alt="background logo" />
                     </div>
                 </div>
             </div>
