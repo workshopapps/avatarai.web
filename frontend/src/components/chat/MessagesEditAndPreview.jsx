00f6a043677337ffb3ca0f7c86f5c2620117b68b
Akuya Ekorot <akuyaekorot@gmail.com>
2022-11-20 14:00:35 +0300

    feat: add modal to chat page

--- frontend/src/components/chat/MessagesEditAndPreview.jsx
+++ frontend/src/components/chat/MessagesEditAndPreview.jsx
@@ -2,17 +2,16 @@ import MessageEditSection from "./MessageEditSection";
 import MessagePreview from "./MessagePreview";
 
 const messages = Array(7).fill("m");
-console.log(messages);
 
 const MessagesEditAndPreview = () => {
   return (
     <div className="border-r border-purple-200">
       <MessageEditSection />
       <div className="flex flex-col py-6 gap-6 border-t border-purple-200">
-        {messages.map((m) => {
+        {messages.map((m, i) => {
           return (
             <MessagePreview
-              key={m}
+              key={i}
               name={`Akuya Ekorot`}
               message={`Can you help me check out this new project i've been working on`}
               time={`43mins`}
