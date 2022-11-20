30f1e290ebeabcdf87a9b7a0a1efa131e980bce1
Akuya Ekorot <akuyaekorot@gmail.com>
2022-11-20 15:24:42 +0300

    fix: cursor pointers

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
+import Faq from "./components/faq";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {/* <App />*/}
+    <Faq />
   </React.StrictMode>
 );
