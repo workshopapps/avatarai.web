7d181e974f7fd091bf49b4e87e8a9be499d27299
Chituru Precious <chiturupreciouspearl@gmail.com>
2022-11-21 21:12:17 +0100

    Feat/Added desktop 4-6

---
+++ frontend/src/components/desktop_6/Desktop6.jsx
@@ -0,0 +1,12 @@
+import Frame_1 from "../desktop_4/Frame_1";
+import Frame_4 from "./Frame_4"
+const  Desktop6= () => {
+    return (
+        <div>
+            <Frame_1 />
+            <Frame_4 />
+        </div>
+      );
+}
+ 
+export default Desktop6;
