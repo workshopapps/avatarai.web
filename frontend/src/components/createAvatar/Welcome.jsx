fb9b5182a2ce4877caa0558b8ff04509df63ecf8
Timi-cell <adedayotimilehin10@gmail.com>
2022-11-21 18:05:12 +0100

    feat:added sidebar and searchfield Components

--- frontend/src/components/createAvatar/Welcome.jsx
+++ frontend/src/components/createAvatar/Welcome.jsx
@@ -2,23 +2,31 @@ import React from "react";
 import Arrow from "./images/icon.svg";
 import Header from "./Header";
 import "./style.css";
+import SearchComponent from "../DashboardSearchComponent/SearchComponent";
+import SidebarEye from "../Desktop_1/SidebarEye";
 
 function Upload() {
   return (
-    <div className="create-avatar">
-      <Header />
-      <div className="upload-block">
-        <img src={Arrow} alt="arrow-right" id="arrow" />
-        <div className="right">
-          <p id="number">2</p>
-          <h2>Upload your Pictures</h2>
-          <p id="upload-text">
-            You have to upload 10 pictures of you in different scenario
-          </p>
-          <p id="add-pictures">
-            Choose pictures from file <br /> (PNG or JPEG)
-          </p>
-          <button type="submit">Proceed</button>
+    <div className="desktop-2">
+      <div>
+        <SidebarEye />
+      </div>
+      <div className="create-avatar">
+        <SearchComponent />
+        <Header />
+        <div className="upload-block">
+          <img src={Arrow} alt="arrow-right" id="arrow" />
+          <div className="right">
+            <p id="number">2</p>
+            <h2>Upload your Pictures</h2>
+            <p id="upload-text">
+              You have to upload 10 pictures of you in different scenario
+            </p>
+            <p id="add-pictures">
+              Choose pictures from file <br /> (PNG or JPEG)
+            </p>
+            <button type="submit">Proceed</button>
+          </div>
         </div>
       </div>
     </div>
