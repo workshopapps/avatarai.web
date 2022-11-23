d67683b5dfa1549dea7cd233a3a696af6dfbe0f2
TheOptimust <ajadimarvellousgo@gmail.com>
2022-11-23 11:41:38 +0100

    .

--- frontend/src/components/desktop_6/Frame_4.jsx
+++ frontend/src/components/desktop_6/Frame_4.jsx
@@ -6,39 +6,53 @@ import Frame_2 from "./Frame_2.png";
 import Frame_3 from "./Frame_3.png";
 import Arrow_0 from "./Arrow_0.png";
 import {Link} from "react-router-dom"
-
+import back from "../DASHBOARD_COMPONENT/dashboardcomp.jsx";
 const Frame_4 = () => {
+
     return ( 
-        <div className="frame_4_bpc">
-            <div className="G_6_bpc">
-                <img src={Group_6} />
-            </div>
-            <h3>Your Avatars are Ready</h3>
+        <div className="opt_w">
+            <img className="G_6_bpc" src={Group_6} />
+            <div className="opt_f">Your Avatars are Ready</div>
             <div className="AV_im_bpc">
-                <div className="img_bpc">
-                <img src={Frame_0} />
+                {/*<div className='opt_b'><img  onClick={handleClick} src={Arrow_0} /></div>*/}
+                <div className="opt_mm">
+                    <img className="img_bpc" src={Frame_0} />
                 </div>
-                <div className="img_bpc">
-                <img src={Frame_1} />
+                <div className="opt_mm">
+                    <img className="img_bpc" src={Frame_0} />
                 </div>
-                <div className="img_bpc">
-                <img src={Frame_2} />
+                <div className="opt_mm">
+                    <img className="img_bpc" src={Frame_0} />
                 </div>
-                <div className="img_bpc">
-                <img src={Frame_3} />
+                <div className="opt_mm">
+                    <img className="img_bpc" src={Frame_0} />
+                </div>
+                <div className="opt_mm">
+                    <img className="img_bpc" src={Frame_0} />
+                </div>
+                <div className="opt_mm">
+                    <img className="img_bpc" src={Frame_0} />
+                </div>
+                <div className="opt_mm">
+                    <img className="img_bpc" src={Frame_0} />
+                </div>
+                <div className="opt_mm">
+                    <img className="img_bpc" src={Frame_0} />
                 </div>
             </div>
-            <div className="G_5_bpc">
-                <img src={Arrow_0} />
-            </div>
-            <div className="SD_btn_bpc">
-                <div className="btn1_bpc">
-                <Link to="#">Share </Link>
+                <div className='opt_q'>
+                    <Link to="#">
+                    <button className="btn1_bpc">
+                        Share
+                    </button>
+                    </Link>
+                    <Link href="#">
+                    <button className="btn2_bpc">
+                        Download
+                    </button>
+                    </Link>
                 </div>
-                <div className="btn2_bpc">
-                <Link href="#">Download</Link>
-                </div>
-            </div>
+
         </div>
      );
 }
