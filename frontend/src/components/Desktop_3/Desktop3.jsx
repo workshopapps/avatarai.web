56cf2af2757f3a0c39bad5a77c1a9465228c3fbf
VeedJohnson <48daveed@gmail.com>
2022-11-23 16:13:52 +0100

    feat(Desktop3): file uploading animation setup

---
+++ frontend/src/components/Desktop_3/Desktop3.jsx
@@ -0,0 +1,14 @@
+import Background from './../DASHBOARD_COMPONENT/dashboardcomp';
+import Content from "./Content";
+
+const Desktop3 = () => {
+  return (
+      <div>
+        <Background
+          content={<Content />}
+        />
+    </div>
+  );
+};
+
+export default Desktop3;
