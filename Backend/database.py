94a1df5890a3ac386f44612ff07b797ba98be2c3
Nwosu Victor <97852276+VicNW@users.noreply.github.com>
2022-11-25 05:14:22 +0400

    Revert "feat/BAC-4-updated get and post images endpoints to s3 bucket for training our dreambooth model."

--- Backend/database.py
+++ Backend/database.py
@@ -1,10 +1,8 @@
 import os
 import motor.motor_asyncio
-import pymongo
 from dotenv import load_dotenv
 
 load_dotenv()
 
 client = motor.motor_asyncio.AsyncIOMotorClient(os.environ["MONGODB_URL"])
-#client = pymongo.MongoClient(os.environ["MONGODB_URL"])
 db = client.hngtest
