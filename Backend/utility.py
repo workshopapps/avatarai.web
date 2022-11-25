2884e661220b2461d24fc4aee8ef3489b0afb866
Ehis Fidelis <meelisfidelis@gmail.com>
2022-11-25 07:32:47 +0100

    user test

--- Backend/utility.py
+++ Backend/utility.py
@@ -11,12 +11,6 @@ JWT_SECRET_KEY = os.environ['JWT_SECRET_KEY']
 JWT_REFRESH_SECRET_KEY = os.environ['JWT_REFRESH_SECRET_KEY']     
 password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def get_hashed_password(password: str) -> str:
-    return password_context.hash(password)
-
-
-def verify_password(password: str, hashed_pass: str) -> bool:
-    return password_context.verify(password, hashed_pass)
 
 
 def create_access_token(subject: Union[str, Any], expires_delta: int = None) -> str:
