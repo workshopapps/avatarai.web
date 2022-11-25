7969b43229becf3ec4ba1675f20294832c85d476
Akuya Ekorot <akuyaekorot@gmail.com>
2022-11-25 18:43:45 +0300

    feat: properly route from dashboard 2,through to dashboard 4 and from dashboard 4, through to dashboard 6

--- frontend/src/components/desktop_5/Desktop5.jsx
+++ frontend/src/components/desktop_5/Desktop5.jsx
@@ -1,7 +1,17 @@
 import Content from "./Content";
 import Background from "../DASHBOARD_COMPONENT/dashboardcomp";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Desktop5 = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigate("/Dashboard_6");
+    }, 5000);
+  });
+
   return (
     <div>
       <Background content={<Content />} />
