e0473d632943421baad528d9848fd50e16fd3056
Headmaster <aliubashirtaiye2002@gmail.com>
2022-11-27 19:37:58 +0100

    Update final.jsx

--- frontend/src/components/landing-page/final/final.jsx
+++ frontend/src/components/landing-page/final/final.jsx
@@ -4,7 +4,7 @@ import './final.css'
 const Final = () => {
   return (
     <div className='final'>
-        <h1>Get started generating amazing AI <span>avatars</span> for yourself</h1>
+        <h1>Get started generating amazing <br/> AI <span>avatars</span> for yourself</h1>
         <button className="final-btn">
         Sign up for free
         </button>
