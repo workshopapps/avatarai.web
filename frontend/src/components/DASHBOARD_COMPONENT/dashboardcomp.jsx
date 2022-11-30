bb8a1575ba377ea0d456cd1c5e2a241d66e1a529
Whalay <soliuy5@gmail.com>
2022-11-30 15:12:55 +0100

    I made the dashboard navbar responsive and fixed

--- frontend/src/components/DASHBOARD_COMPONENT/dashboardcomp.jsx
+++ frontend/src/components/DASHBOARD_COMPONENT/dashboardcomp.jsx
@@ -13,8 +13,9 @@ export default function Dashboardcomp(props) {
         <div className="vic_dash_inner_div  ">
       
       
-         <div className="vic_dash_header mb-10 ">
-            <div className="vic_dash_input_div ">
+        <div className="w-[95vw] h-20  bg-[#fafafa] absolute top-0 right-0">
+        <div className="vic_dash_header mb-10 mt-5 md:w-[90vw] lg:max-w-screen-lg ">
+            <div className="vic_dash_input_div md:ml-8 lg:ml-24 ">
               <img src={Search} className="vic_search" />
               <input
                 type="text"
@@ -30,6 +31,7 @@ export default function Dashboardcomp(props) {
           
           </div>
          </div>
+        </div>
           {/* <div className="vic_dash_text_div">
             <h2 className="vic_dash_h2">Welcome</h2>
             <p className="vic_dash_p">Let's create your Avatars.</p>
