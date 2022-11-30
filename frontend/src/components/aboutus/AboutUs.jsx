a844618cbe072d5c4664c75578416ef362d588ea
chizycodes <okwudirejoy@gmail.com>
2022-11-30 04:59:19 +0100

    feat/large image section

--- frontend/src/components/aboutus/AboutUs.jsx
+++ frontend/src/components/aboutus/AboutUs.jsx
@@ -1,17 +1,18 @@
-import WhyUsCard from "./WhyUsCard";
-import TeamMemberCard from "./TeamMemberCard";
-import "./aboutUs.css";
-import Banner from "./img/aboutBg.png";
-import PolygonLeft from "./img/polygonLeft.png";
-import PolygonRight from "./img/polygonRight.png";
-import Navbar from "../landingPage/Navbar/Navbar";
+import WhyUsCard from './WhyUsCard';
+import TeamMemberCard from './TeamMemberCard';
+import './aboutUs.css';
+import Banner from './img/aboutBg.png';
+import PolygonLeft from './img/polygonLeft.png';
+import PolygonRight from './img/polygonRight.png';
+import Navbar from '../landingPage/Navbar/Navbar';
+import Banner2 from './img/image1.svg';
 
-import { WHY_US, TEAM } from "./data";
+import { WHY_US, TEAM } from './data';
 // import Footer from "../Footerpage/Footer";
-import Foooter from "../footer/Foooter";
+import Foooter from '../footer/Foooter';
 
 const AboutUs = () => {
-  return (
+	return (
 		<>
 			<Navbar />
 			<div id="dja_main text-[#333333]">
@@ -29,14 +30,14 @@ const AboutUs = () => {
 					<div className="dja_overlay"></div>
 				</section>
 
-				<section className="py-[48px]">
-					<div className="flex justify-between gap-4 lg:gap-5 max-w-[1200px] mx-auto">
-						<h2 className="text-[#333333] font-semibold text-[40px] lg:text-[64px] lg:leading-[81px] max-w[611px]">
+				<section className="py-3 lg:py-[48px] px-5">
+					<div className="flex justify-between items-start gap-3 md:gap-5 max-w-[1200px] mx-auto ">
+						<h2 className="text-[#333333] font-semibold text-[20px] lg:text-[64px] leading-[20px] lg:leading-[81px] flex-auto w-56 md:w-64">
 							We are on a mission to change <span className="text-[#808080]">the face of art creation.</span>
 						</h2>
-						<p className="font-normal flex flex-col items-center max-w-[518px] mt-[40px] lg:mt-[80px]">
+						<p className="font-normal flex flex-col items-center mt-[40px] lg:mt-[80px] flex-auto w-44 md:w-32">
 							<span className="text-[#808080] text-[48px] md:text-[64px] leading-[0px]">“</span>
-							<span className="text-[14px] lg:text-[24px] font-normal">
+							<span className="text-[10px] lg:text-[24px] font-normal">
 								Having known that AI Art has been a thing for years, the team thought "maybe we can create a software
 								that can generate avatars of people in different environments.
 							</span>
@@ -45,12 +46,24 @@ const AboutUs = () => {
 					</div>
 				</section>
 
-				<section className="dja_story_section">
-					<div className="dja_story_design_wrap" id="dja_design_1">
-						<img src={PolygonLeft} alt="story" />
-					</div>
-					<div className="dja_story_design_wrap" id="dja_design_2">
-						<img src={PolygonRight} alt="story" />
+				<section className="w-full">
+					<img src={Banner2} alt="Large banner" className="w-full" />
+				</section>
+
+				<section className="py-5 lg:py-[48px] px-5">
+					<div className="flex justify-between gap-3 md:gap-5 max-w-[1200px] mx-auto ">
+						<p className="font-normal flex flex-col items-center mt-auto lg:mt-[80px] flex-auto max-w-[413px]">
+							<span className="text-[#808080] text-[48px] md:text-[64px] leading-[0px]">“</span>
+							<span className="text-[10px] lg:text-[24px] font-normal">
+								With Zuvatar, users get to create avatars and protect their physical identity on any social platform and
+								maintain anonymity in a virtual world.
+							</span>
+							<span className="text-[#808080] text-[48px] md:text-[64px] leading-4 mt-7">”</span>
+						</p>
+						<h2 className="text-[#333333] font-semibold text-[16px] mt-[60px] lg:text-[64px] leading-[16px] lg:leading-[81px] flex-auto md:w-64">
+							"Art privacy is crucial, and <span className="text-[#808080]">Zuvatar</span> is making it{' '}
+							<span className="text-[#808080]">possible</span>" - <span className="text-[#808080]">Team Tape</span>.
+						</h2>
 					</div>
 				</section>
 
