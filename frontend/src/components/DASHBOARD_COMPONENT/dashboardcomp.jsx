d6c0041e64de580f9d59c382efa04ffa030211ff
Whalay <soliuy5@gmail.com>
2022-11-30 14:15:01 +0100

    I made changes to the Dashboard 2, I change the progressbar

--- frontend/src/components/DASHBOARD_COMPONENT/dashboardcomp.jsx
+++ frontend/src/components/DASHBOARD_COMPONENT/dashboardcomp.jsx
@@ -9,10 +9,12 @@ export default function Dashboardcomp(props) {
   return (
     <main className="aso-dashboard-comp-main">
       <Sidebar />
-      <div className="vic_dash_bg">
-        <div className="vic_dash_inner_div">
-          <div className="vic_dash_header">
-            <div className="vic_dash_input_div">
+      <div className="vic_dash_bg ">
+        <div className="vic_dash_inner_div  ">
+      
+      
+         <div className="vic_dash_header mb-10 ">
+            <div className="vic_dash_input_div ">
               <img src={Search} className="vic_search" />
               <input
                 type="text"
@@ -21,16 +23,17 @@ export default function Dashboardcomp(props) {
               />
               <img src={Mic} className="vic_mic" />
             </div>
-            <div className="vic_dash_translate">
+            <div className="vic_dash_translate ">
               <p>English</p>
               <img src={Arrow} onClick="" />
               {/* translation languages come here and will be linked to the onClick event above  */}
-            </div>
+          
           </div>
-          <div className="vic_dash_text_div">
+         </div>
+          {/* <div className="vic_dash_text_div">
             <h2 className="vic_dash_h2">Welcome</h2>
             <p className="vic_dash_p">Let's create your Avatars.</p>
-          </div>
+          </div> */}
           <div className="vic_dash_content">{props.content}</div>
         </div>
       </div>
