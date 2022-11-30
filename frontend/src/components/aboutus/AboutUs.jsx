74eb53c797bf25b394890f992b2cd1f149e8c2e0
chizycodes <okwudirejoy@gmail.com>
2022-11-30 05:40:19 +0100

    fix: responsiveness

--- frontend/src/components/aboutus/AboutUs.jsx
+++ frontend/src/components/aboutus/AboutUs.jsx
@@ -31,43 +31,53 @@ const AboutUs = () => {
 				</section>
 
 				<section className="py-3 lg:py-[48px] px-5">
-					<div className="flex justify-between items-start gap-3 md:gap-5 max-w-[1200px] mx-auto ">
-						<h2 className="text-[#333333] font-semibold text-[20px] lg:text-[64px] leading-[20px] lg:leading-[81px] flex-auto w-56 md:w-64">
-							We are on a mission to change <span className="text-[#808080]">the face of art creation.</span>
-						</h2>
-						<p className="font-normal flex flex-col items-center mt-[40px] lg:mt-[80px] flex-auto w-44 md:w-32">
-							<span className="text-[#808080] text-[48px] md:text-[64px] leading-[0px]">“</span>
-							<span className="text-[10px] lg:text-[24px] font-normal">
-								Having known that AI Art has been a thing for years, the team thought "maybe we can create a software
-								that can generate avatars of people in different environments.
-							</span>
-							<span className="text-[#808080] text-[48px] md:text-[64px] leading-4 mt-7">”</span>
-						</p>
+					<div className="lg:h-[300px] flex justify-between items-start gap-3 md:gap-5 max-w-[1200px] mx-auto ">
+						<div className="flex-auto w-56 md:w-64 flex h-full">
+							<h2 className="text-[#333333] font-semibold text-[20px] lg:text-[64px] leading-[20px] lg:leading-[81px]">
+								We are on a mission to change <span className="text-[#808080]">the face of art creation.</span>
+							</h2>
+						</div>
+
+						<div className="flex-auto w-44 md:w-32 flex items-end h-full">
+							<p className="font-normal flex flex-col items-center mt-[40px] lg:mt-[80px]">
+								<span className="text-[#808080] text-[48px] md:text-[64px] leading-[0px]">“</span>
+								<span className="text-[10px] lg:text-[24px] font-normal">
+									Having known that AI Art has been a thing for years, the team thought "maybe we can create a software
+									that can generate avatars of people in different environments.
+								</span>
+								<span className="text-[#808080] text-[48px] md:text-[64px] leading-4 mt-7">”</span>
+							</p>
+						</div>
 					</div>
 				</section>
 
-				<section className="w-full">
+				<div className="w-full pb-5">
 					<img src={Banner2} alt="Large banner" className="w-full" />
-				</section>
+				</div>
 
-				<section className="py-5 lg:py-[48px] px-5">
-					<div className="flex justify-between gap-3 md:gap-5 max-w-[1200px] mx-auto ">
-						<p className="font-normal flex flex-col items-center mt-auto lg:mt-[80px] flex-auto max-w-[413px]">
-							<span className="text-[#808080] text-[48px] md:text-[64px] leading-[0px]">“</span>
-							<span className="text-[10px] lg:text-[24px] font-normal">
-								With Zuvatar, users get to create avatars and protect their physical identity on any social platform and
-								maintain anonymity in a virtual world.
-							</span>
-							<span className="text-[#808080] text-[48px] md:text-[64px] leading-4 mt-7">”</span>
-						</p>
-						<h2 className="text-[#333333] font-semibold text-[16px] mt-[60px] lg:text-[64px] leading-[16px] lg:leading-[81px] flex-auto md:w-64">
-							"Art privacy is crucial, and <span className="text-[#808080]">Zuvatar</span> is making it{' '}
-							<span className="text-[#808080]">possible</span>" - <span className="text-[#808080]">Team Tape</span>.
-						</h2>
+				<section className="pt-3 lg:pt-[48px] px-5">
+					<div className="lg:h-[300px] flex justify-between gap-3 md:gap-5 max-w-[1200px] mx-auto border-b border-[#808080] pb-3 lg:pb-[48px]">
+						<div className="flex-auto max-w-[413px] flex items-end h-full">
+							<p className="font-normal flex flex-col items-center">
+								<span className="text-[#808080] text-[48px] md:text-[64px] leading-[0px]">“</span>
+								<span className="text-[10px] lg:text-[24px] font-normal">
+									With Zuvatar, users get to create avatars and protect their physical identity on any social platform
+									and maintain anonymity in a virtual world.
+								</span>
+								<span className="text-[#808080] text-[48px] md:text-[64px] leading-4 mt-7">”</span>
+							</p>
+						</div>
+
+						<div className="flex-auto md:w-64 flex h-full">
+							<h2 className="text-[#333333] font-semibold text-[16px] lg:text-[64px] leading-[16px] lg:leading-[81px]">
+								"Art privacy is crucial, and <span className="text-[#808080]">Zuvatar</span> is making it{' '}
+								<span className="text-[#808080]">possible</span>" - <span className="text-[#808080]">Team Tape</span>.
+							</h2>
+						</div>
 					</div>
 				</section>
 
-				<section className="dja_why_section">
+				{/* <section className="dja_why_section">
 					<div className="dja_container">
 						<h2 className="dja_title">Why Us?</h2>
 						<div className="dja_why_ctn">
@@ -76,7 +86,7 @@ const AboutUs = () => {
 							))}
 						</div>
 					</div>
-				</section>
+				</section> */}
 
 				<section className="dja_team_section">
 					<div className="dja_container">
