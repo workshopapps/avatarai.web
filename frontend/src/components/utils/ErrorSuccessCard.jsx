d6fd84d88b89801470e107889a9897eb79cdfe0c
chizycodes <okwudirejoy@gmail.com>
2022-12-02 07:01:55 +0100

    feat/ auth error/success notification

---
+++ frontend/src/components/utils/ErrorSuccessCard.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+const ErrorSuccessCard = ({ error, message }) => {
+	return (
+		<div
+			className={`alert ${
+				!error ? 'bg-white border-green-600 text-green-600' : 'bg-white border-red-600 text-red-600'
+			} shadow-md border mb-5`}
+		>
+			<div>
+				{!error ? (
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						className="stroke-green-600 flex-shrink-0 h-6 w-6"
+						fill="none"
+						viewBox="0 0 24 24"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							strokeWidth="2"
+							d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+						/>
+					</svg>
+				) : (
+					<svg
+						xmlns="http://www.w3.org/2000/svg"
+						className="stroke-red-600 flex-shrink-0 h-6 w-6"
+						fill="none"
+						viewBox="0 0 24 24"
+					>
+						<path
+							strokeLinecap="round"
+							strokeLinejoin="round"
+							strokeWidth="2"
+							d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+						/>
+					</svg>
+				)}
+
+				<span className={!error ? 'text-green-600' : 'text-red-600'}>
+					{message}
+				</span>
+			</div>
+		</div>
+	);
+};
+
+export default ErrorSuccessCard;
--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,22 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./src/**/*.{js,jsx,ts,tsx}",
-    ],
-    theme: {
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				nunito: ['Nunito'],
+				'nunito-sans': ['Nunito Sans'],
+				jakarta: ["'Plus Jakarta Sans'"],
+				inter: ['Inter'],
+			},
+			// colors: {
+			//     'purple': "#8B70E9",
 
-        extend: {
-            fontFamily: {
-                nunito: ["Nunito"],
-                "nunito-sans": ["Nunito Sans"],
-                jakarta: ["'Plus Jakarta Sans'"],
-                inter: ["Inter"],
-            },
-            // colors: {
-            //     'purple': "#8B70E9",
-
-            // },
-        },
-    },
-    plugins: [],
-}
+			// },
+		},
+	},
+	plugins: [require('daisyui')],
+	daisyui: {
+		themes: ['light'],
+	},
+};
