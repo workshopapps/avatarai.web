de3f9dbc96d1c685eed502b5c3e42b5ff6f4936d
Timi-cell <adedayotimilehin10@gmail.com>
2022-12-02 23:55:07 +0100

    feat/changed the dashboard background and implemented the upload instructions modal

--- frontend/src/components/DASHBOARD_COMPONENT/TopNav.jsx
+++ frontend/src/components/DASHBOARD_COMPONENT/TopNav.jsx
@@ -2,33 +2,48 @@ import React from 'react';
 import MenuIcon from './img/menu.svg';
 import Logo from './img/logo.svg';
 import Dashboardtranslate from './dashboardtranslate';
+import ArrowDown from "./img/arrowdown.svg";
 
 export const TopNav = ({ profile, setProfile, show, setShow }) => {
 	return (
-		<div className="mb-[60px]">
-			<nav className="pl-6">
-				<div  className='flex items-center justify-between mb-[40px] xl:mb-0 '>
-					<div className="">
-						<div className="text-gray-600 visible xl:hidden relative" onClick={() => setShow(!show)}>
-							{show ? ' ' : <img src={MenuIcon} alt="Menu" />}
-						</div>
-					</div>
+    <div className="mb-[60px]">
+      <nav className="pl-6">
+        <div className="flex items-center justify-between mb-[40px] xl:mb-0 ">
+          <div className="">
+            <div
+              className="text-gray-600 visible xl:hidden relative"
+              onClick={() => setShow(!show)}
+            >
+              {show ? " " : <img src={MenuIcon} alt="Menu" />}
+            </div>
+          </div>
 
-					<img src={Logo} alt="Logo" className="xl:hidden block" />
+          <img src={Logo} alt="Logo" className="xl:hidden block" />
 
-					<div></div>
-				</div>
+          <div></div>
+        </div>
 
-				<div className="flex items-center justify-between gap-5">
-					<div>
-						<h1 className="text-[24px] md:text-[40px] text-[#000] font-bold">Hello Baki,</h1>
-						<p className="text-[#6c6c6c] text-sm md:text-[18px]">Start generating cool avatars.</p>
-					</div>
-					<div>
-						<Dashboardtranslate />
-					</div>
-				</div>
-			</nav>
+        <div className="flex items-center justify-between gap-5">
+          <div>
+            <h1 className="text-[24px] md:text-[40px] text-[#000] font-bold">
+              Hello Baki,
+            </h1>
+            <p className="text-[#6c6c6c] text-sm md:text-[18px]">
+              Start generating cool avatars.
+            </p>
+          </div>
+
+          <div>
+            <Dashboardtranslate />
+          </div>
+        </div>
+        <div className='flex items-center gap-5 mt-[40px]'>
+			<p className="text-[#000] text-sm md:text-[20px] font-bold">
+			  Read upload instructions
+			</p>
+			<img src={ArrowDown} alt="arrow-down" />
 		</div>
-	);
+      </nav>
+    </div>
+  );
 };
