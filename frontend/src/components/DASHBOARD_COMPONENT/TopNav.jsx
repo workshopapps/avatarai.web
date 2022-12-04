a87691061a07c4c40b2f1b21f193623e4f5f0dbd
Timi-cell <adedayotimilehin10@gmail.com>
2022-12-04 20:43:01 +0100

    added the instruction modal

--- frontend/src/components/DASHBOARD_COMPONENT/TopNav.jsx
+++ frontend/src/components/DASHBOARD_COMPONENT/TopNav.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
-import MenuIcon from './img/menu.svg';
-import Logo from './img/logo.svg';
-import Dashboardtranslate from './dashboardtranslate';
+import React from "react";
+import MenuIcon from "./img/menu.svg";
+import Logo from "./img/logo.svg";
+import Dashboardtranslate from "./dashboardtranslate";
 import ArrowDown from "./img/arrowdown.svg";
-
+import "./instruct.css";
+import { useState } from "react";
 export const TopNav = ({ profile, setProfile, show, setShow }) => {
-	return (
+  const [modal, showModal] = useState(true);
+  const showUp = () => {
+    showModal((modal) => !modal);
+  };
+  return (
     <div className="mb-[60px]">
       <nav className="pl-6">
         <div className="flex items-center justify-between mb-[40px] xl:mb-0 ">
@@ -37,12 +42,46 @@ export const TopNav = ({ profile, setProfile, show, setShow }) => {
             <Dashboardtranslate />
           </div>
         </div>
-        <div className='flex items-center gap-5 mt-[40px]'>
-			<p className="text-[#000] text-sm md:text-[20px] font-bold">
-			  Read upload instructions
-			</p>
-			<img src={ArrowDown} alt="arrow-down" />
-		</div>
+        <div className="flex items-center gap-3 mt-[40px]">
+          <p className="text-[#000] text-sm md:text-[20px] font-bold">
+            Read upload instructions
+          </p>
+          <img src={ArrowDown} alt="arrow-down" onClick={showUp} />
+        </div>
+
+        <div
+          className={`flex items-center p-5 my-5 gap-5  bg-[#fff] ${
+            modal ? "hide" : ""
+          }`}
+        >
+          <div>
+            <p className="text-sm my-1.5">✅ No other people in your photos</p>
+            <p className="text-sm my-1.5">
+              ✅ Photos with different expression
+            </p>
+            <p className="text-sm my-1.5">
+              ✅ Photos in different locations, backgrounds and angles
+            </p>
+            <p className="text-sm my-1.5">✅ Only images - 512x512px</p>
+            <p className="text-sm my-1.5">
+              ✅ Photos at different times of day
+            </p>
+            <p className="text-sm my-1.5">
+              ✅ Look into the camera and also away
+            </p>
+            <p className="text-sm my-1.5">
+              ✅ If couple, make sure EVERY photo has BOTH of you in it
+            </p>
+          </div>
+          <div>
+            <p className="text-sm my-1.5">❌ No duplicate photos</p>
+            <p className="text-sm my-1.5">❌ No black and white photos</p>
+            <p className="text-sm my-1.5">❌ No photo shoots</p>
+            <p className="text-sm my-1.5">❌ Not only selfies</p>
+            <p className="text-sm my-1.5">❌ No children</p>
+            <p className="text-sm my-1.5">❌ No nudes</p>
+          </div>
+        </div>
       </nav>
     </div>
   );
