9bda837680860f665f022792c6496347529fb1ba
chelsea-banke <chelseanewhorizon@gmail.com>
2022-12-05 16:23:04 +0100

    making delete button display only on picture preview hover

--- frontend/src/components/desktop_2/DD2Content.jsx
+++ frontend/src/components/desktop_2/DD2Content.jsx
@@ -235,10 +235,62 @@ const ImageUpload = () => {
                     // onChange={storeItem(image)}
                   >
                     <img src={image} className="vic_her" />
+
+                    <button
+                      id="closeSideBar"
+                      className=" mr-8 vic_x"
+                      onClick={() =>
+                        setSelectedImages(
+                          selectedImages.filter((e) => e !== image)
+
+                        )
+                      }
+                     
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="icon icon-tabler icon-tabler-x"
+                        width={30}
+                        height={30}
+                        viewBox="0 0 24 24"
+                        strokeWidth="1.5"
+                        stroke="white"
+                        fill="none"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        key={image}
+                      >
+                        <path stroke="none" d="M0 0h24v24H0z" />
+                        <line x1={18} y1={6} x2={6} y2={18} />
+                        <line x1={6} y1={6} x2={18} y2={18} />
+                      </svg>
+                    </button>
                   </div>
                 );
               })}
-              
+
+            <form action="" className="vic-dd2-form">
+              <div className="aso-dd2-input">
+                
+                <input
+                  accept="image/*"
+                  multiple
+                  type="file"
+                  name="file"
+                  id="file"
+                  // onclick={setPreview(false)}
+                  onChange={handleFilee}
+                 
+                />
+                  
+                <label htmlFor="file"
+                
+                >
+                  {/* <label htmlFor="file" className="aso-dd2-label"> */}
+                  <img src={upload} alt="" />
+                </label>
+              </div>
+            </form>
           </div>
           {/* <img src={right} className="vic_right" /> */}
           {/* </div> */}
