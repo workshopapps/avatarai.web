5348178856147cd616e0975e2aeecc66b50aab7e
ANAGU DANIEL <anagudaniel@gmail.com>
2022-12-07 22:24:16 -0800

    style

--- frontend/src/components/LoginPage/CheckEmail.jsx
+++ frontend/src/components/LoginPage/CheckEmail.jsx
@@ -14,26 +14,28 @@ const CheckEmail = () => {
   return (
     <div className="flex flex-col pt-[120px] w-[100vw] md:p-0 md:justify-center items-center h-screen">
       <div className="flex flex-col w-full max-w-xl px-6 gap-6 md:gap-8 items-center justify-center">
-        <div className="">
+        <div>
           <img className="w-[55%] ml-[30%]" src={mail_opt} alt="" />
         </div>
+        <div className='w-[100%] content-center justify-center '>
         <div className="flex flex-col gap-2 text-center">
-          <h1 className="font-bold text-[#333333] text-xl md:text-4xl">
+          <h1 className=" text-center font-bold text-[#333333] text-xl md:text-4xl text-center">
             Check your email address
           </h1>
         </div>
-        
-        <p className=" text-[22px] text-center md:text-center md:w-[140%] text-[#808080] text-[32px] mt-[-2%] font-[500] tracking-[0.2px]">
+        <div>
+        <p className=" pb-[5%] pt-[5%] text-[22px] text-center md: pb-[5%] text-center md:w-[100%] pt-[5%] text-[#808080] text-[32px] mt-[-2%] font-[500] tracking-[0.2px]">
         A password reset link has been sent to <span className='text-[#6C6191] font-[700]'>{emailForgot}</span> click on the link to reset your password.
           </p>
+        </div>
           <div>
-            <div className='w-[100%]'>
-          <button className=" items-center content-center justify-center mix-auto p-[12px] rounded-[8px] w-[100%] md:items-center content-center  justify-center mix-auto rounded-[8px] p-[12px] w-[100%] bg-[#8B70E9] md: text-white sm: w-[50%] ">
+            <div className='w-[100%] justify-center content-center'>
+          <button className=" w-[100%] items-center content-center justify-center p-[12px] rounded-[8px] md:w-[60%] justify-center rounded-[8px] md:ml-[20%] p-[12px] bg-[#8B70E9] md:text-white sm:w-[60%]">
           <Link to="/reset-password">Continue</Link>
           </button>
           </div>
           <div className='ml-[-50%]'>
-          <p className=" w-[130%] text-[#808080] text-center mt-[6%] md:w-[130%]  text-[15px] text-center">
+          <p className=" w-[130%] text-[#808080] text-center mt-[4%] md:w-[130%]  text-[15px] text-center">
           Didn't receive the e-mail?{""}
           <a href="" className=" md:font-[700] text-[#8B70E9] text-[15px] ">
             Resend
@@ -41,6 +43,8 @@ const CheckEmail = () => {
         </p>
           </div>
           </div>
+        </div>
+        
         
         
       </div>
