140f747a08211418fb22af47eecca1e9a3d83b03
Headmaster <aliubashirtaiye2002@gmail.com>
2022-12-15 09:36:30 +0100

    Landing Page Refix

--- frontend/src/components/landing-page/question/FaqGetInTouch.jsx
+++ frontend/src/components/landing-page/question/FaqGetInTouch.jsx
@@ -5,7 +5,7 @@ const FaqGetInTouch = () => {
   return (
     <div className="flex flex-col gap-[42px] items-center align-center">
       <Link to="/FAQ" className="flex items-center w-full">
-        <Button className="cursor-pointer text-white m-auto font-semibold flex items-center justify-center h-[48px] w-[100%] md:w-[150px] ld:w-[150px] bg-[#8B70E9] rounded">
+        <Button className="cursor-pointer text-white m-auto font-semibold flex my-5 items-center justify-center h-[48px] w-[100%] md:w-[150px] ld:w-[150px] bg-[#8B70E9] rounded">
           <p data-testid="button" className="text-white text-[16px]">
             View All
           </p>
